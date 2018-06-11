a971addb90243937917fade4e4d1a3e3cee0f8a1
Congboss <congqian62@126.com>
2018-06-11 22:37:12 +0800

    Initial commit

---
+++ src/test/java/com/qiancong/sell/dao/ProductCategoryRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.qiancong.sell.dao;
+
+import com.qiancong.sell.dataobject.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+@Test
+public void findOneTest(){
+    ProductCategory productCategory=productCategoryRepository.findById(1).get();
+    System.out.println(productCategory.toString());
+}
+@Test
+@Transactional
+public void saveTest(){
+    ProductCategory productCategory=new ProductCategory("男生最爱",5);
+    ProductCategory result=productCategoryRepository.save(productCategory);
+    Assert.assertNotEquals(null,result);
+}
+@Test
+    public void updateTest(){
+    ProductCategory productCategory=productCategoryRepository.findById(2).get();
+    productCategory.setCategoryType(23);
+    productCategoryRepository.save(productCategory);
+}
+@Test
+    public void findByCategoryTypeInTest(){
+    List<Integer> list= Arrays.asList(2,3,4);
+    List<ProductCategory> result=productCategoryRepository.findByCategoryTypeIn(list);
+    Assert.assertNotEquals(0,result.size());
+
+}
+}
